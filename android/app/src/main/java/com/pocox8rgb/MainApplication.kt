0c5af69876f8ddfcf82819329fab40378e9ce16a
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-04-14 11:00:25 -0300

    feat: Add PocoNotificationPackage to MainApplication

---
+++ android/app/src/main/java/com/pocox8rgb/MainApplication.kt
@@ -0,0 +1,8 @@
+override fun getPackages(): List<ReactPackage> =
+    PackageList(this).packages.apply {
+        // Packages that cannot be autolinked yet can be added manually here, for example:
+        // add(MyReactNativePackage())
+        
+        // ADD YOUR PACKAGE HERE:
+        add(PocoNotificationPackage())
+    }
